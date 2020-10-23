7f929e15b10d65e3d38152cf67d8d49d05c493f7
nickrotondo <nickrotondo@gmail.com>
2020-10-22 17:03:43 -0700

    Add minor query optimization

--- database/dbHelpers-mongo.js
+++ database/dbHelpers-mongo.js
@@ -2,7 +2,19 @@ const db = require('./index-mongo.js');
 
 module.exports = {
   getAllReviewsForItem: (productId, callback) => {
-    db.Review.find({ productOrShopId: productId }).exec((err, results) => {
+    db.Review.find({
+      productOrShopId: productId,
+    }, {
+      id: 1,
+      productOrShopId: 1,
+      userName: 1,
+      userPhoto: 1,
+      reviewDate: 1,
+      style: 1,
+      review: 1,
+      reviewPic: 1,
+      reviewRating: 1,
+    }).hint({ productOrShopId: 1 }).exec((err, results) => {
       if (err) {
         callback(err);
       } else {
