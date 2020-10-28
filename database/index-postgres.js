3c8a2eda508dc42fe045ce29ad2a4cc308245c6b
nickrotondo <nickrotondo@gmail.com>
2020-10-28 09:57:58 -0700

    Add connection info for deployed database

--- database/index-postgres.js
+++ database/index-postgres.js
@@ -1,10 +1,40 @@
 const { Pool } = require('pg');
 
+// // Local:
+// const pool = new Pool({
+//   database: 'getsy',
+//   port: 5432,
+// });
+
+// EC2:
 const pool = new Pool({
-  database: 'getsy',
+  host: 'IP_ADDRESS',
   port: 5432,
+  user: 'nickrotondo',
+  password: 'PASSWORD',
+  database: 'getsy',
 });
 
 module.exports = {
   query: (text, callback) => pool.query(text, callback),
 };
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   host: 'IP_ADDRESS',
+//   port: 5432,
+//   user: 'nickrotondo',
+//   password: 'PASSWORD',
+//   database: 'getsy',
+// });
+
+// client.connect((err) => {
+//   if (err) {
+//     console.error('connection error', err.stack);
+//   } else {
+//     console.log('connected');
+//   }
+// });
+
+// module.exports = client;
